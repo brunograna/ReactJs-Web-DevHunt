a35893e17e30632971073312ef3a7a1872c6cc28
Bruno Garcia Rana <brunogarciarana@gmail.com>
2020-03-24 19:57:36 -0300

    Starting ReactJs

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, {useState} from 'react';
+
+import Header from './Header';
+// Tres conceitos
+// Componente: Bloco isolado de HTML, CSS e JS, o qual não interfere no restante da aplicação
+// Propriedade: Informações que o componente PAI passa para o componente filho
+// Estado: Informações mantidas pelo componente (Lembrar: Imutabilidade)
+
+function App() {
+    const [counter, setCounter] = useState(0);
+
+    function incrementCounter() {
+        setCounter(counter + 1);
+    }
+
+    return (
+    <>
+        <h1>Contador {counter}</h1>
+        <button onClick={incrementCounter}>Incrementar</button>
+    </>
+  );
+}
+
+export default App;
